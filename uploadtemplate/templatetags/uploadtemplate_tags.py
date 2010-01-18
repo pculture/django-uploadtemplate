339668970ddbc575d34e3e5ed6a325d18a250454
Paul Swartz <paulswartz@gmail.com>
2010-01-18 11:41:56 -0500

    cache the current theme in the template

--- uploadtemplate/templatetags/uploadtemplate_tags.py
+++ uploadtemplate/templatetags/uploadtemplate_tags.py
@@ -7,13 +7,27 @@ from uploadtemplate import models
 
 register = template.Library()
 
-@register.simple_tag
-def get_static_url(path):
-    try:
-        theme = models.Theme.objects.get_default()
-    except models.Theme.DoesNotExist:
-        base = settings.MEDIA_URL
-    else:
-        base = theme.static_url()
+class ThemeStaticUrlNode(template.Node):
+    def __init__(self, path):
+        self.path = path
 
-    return urlparse.urljoin(base, path)
+    def render(self, context):
+        if 'uploadtemplate_theme' in context:
+            theme = context['uploadtemplate_theme']
+            base = theme.static_url()
+        else:
+            try:
+                theme = models.Theme.objects.get_default()
+            except models.Theme.DoesNotExist:
+                base = settings.MEDIA_URL
+            else:
+                context['uploadtemplate_theme'] = theme
+                base = theme.static_url()
+        return urlparse.urljoin(base, self.path.resolve(context))
+
+@register.tag
+def get_static_url(parser, token):
+    tokens = token.split_contents()
+    if len(tokens) != 2:
+        raise template.TemplateSyntaxError('%s takes 1 argument' % tokens[0])
+    return ThemeStaticUrlNode(template.Variable(tokens[1]))
