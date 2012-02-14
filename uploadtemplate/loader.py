37db5df827cee4ee09e6f9ca6aaaf60240c757ce
Paul Swartz <paulswartz@gmail.com>
2012-02-14 16:35:52 -0500

    upgrade template loader to a class

--- uploadtemplate/loader.py
+++ uploadtemplate/loader.py
@@ -3,12 +3,24 @@ from django.template.loaders import filesystem
 
 from uploadtemplate import models
 
-def load_template_source(template_name, dirs=None):
-    try:
-        theme = models.Theme.objects.get_default()
-    except models.Theme.DoesNotExist:
-        raise TemplateDoesNotExist, 'no default theme'
+class Loader(filesystem.Loader):
+    def load_template_source(self, template_name, dirs=None):
+        try:
+            theme = models.Theme.objects.get_default()
+        except models.Theme.DoesNotExist:
+            raise TemplateDoesNotExist, 'no default theme'
 
-    return filesystem.load_template_source(template_name,
-                                           [theme.template_dir()])
+        return filesystem.Loader.load_template_source(
+            self, template_name, [theme.template_dir()])
+
+    load_template_source.is_usable = True
+
+_loader = Loader()
+
+def load_template_source(template_name, dirs=None):
+    import warning
+    warning.warn("`uploadtemplate.loader.load_template_source` is deprecated. "
+                 "Use `uploadtempalte.loader.Loader` instead.",
+                 DeprecationWarning)
+    return _loader.load_template_source(template_name, dirs)
 load_template_source.is_usable = True
